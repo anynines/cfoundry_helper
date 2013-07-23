305334657cf74508126055f073e409db3a2253f7
Julian Weber <jweber@avarteq.de>
2013-07-23 12:58:24 +0200

    adds user deletion to the UserHelper

--- lib/cfoundry_helper/helpers/user_helper.rb
+++ lib/cfoundry_helper/helpers/user_helper.rb
@@ -1,6 +1,7 @@
 module CFoundryHelper::Helpers
   module UserHelper
 
+    # returns the associated email address for the given cc user
     def self.get_email_for_user(user)
       guid = user.guid
 
@@ -15,6 +16,7 @@ module CFoundryHelper::Helpers
       query_result["resources"].first["emails"].first["value"]
     end
 
+    # creates a user with the given email and password
     def self.create_user(attributes)
       raise "The given attributes hash is nil!" if attributes.nil?
       raise "No user email given!" if attributes[:email].nil?
@@ -36,10 +38,26 @@ module CFoundryHelper::Helpers
       user
     end
 
+    # deletes a user from the cc and the uaa
+    # returns true if the user was deleted
+    # throws an exception on errors
+    def self.delete_user(user)
+      self.delete_user_from_uaa user
+      self.delete_user_from_cc  user
+      return true
+    end
+
+    def self.delete_user_by_email(email)
+      user = self.get_user_by_email email
+      self.delete_user user
+    end
+
+    # returns an array of CFoundry::V2::User registered within the cloud controller
     def self.get_cc_users
       CFoundryHelper::Helpers::ClientHelper.cloud_controller_client.users
     end
 
+    # returns an array of all user emails registered within the uaa
     def self.get_user_emails
       scim_cl = CFoundryHelper::Helpers::ClientHelper.scim_client
       query_result = scim_cl.query(:user)
@@ -57,6 +75,7 @@ module CFoundryHelper::Helpers
       res_array
     end
 
+    # checks whether a user with the given email address exists
     def self.email_exists?(email)
       begin
         u = self.get_user_by_email email
@@ -94,6 +113,35 @@ module CFoundryHelper::Helpers
         raise "Could not find the user with the email address: #{email} in the Cloud Controller!"
       end
       user
+    end
+
+    # sets a new password for the given user
+    # returns the user
+    def self.set_user_password(user, newpass)
+      scim_cl = CFoundryHelper::Helpers::ClientHelper.scim_client
+      scim_cl.change_password user.guid, newpass, nil
+      return user
+    end
+
+
+
+    private
+
+    # deletes a user from the uaa
+    # returns true if the user was deleted
+    def self.delete_user_from_uaa(user)
+      raise "The given user is nil!" if user.nil?
+      user_id = user.guid
+
+      scim_cl = CFoundryHelper::Helpers::ClientHelper.scim_client
+      result = scim_cl.delete(:user, user_id)
+      return true
+    end
+
+    # deletes a user from the cloud controller
+    def self.delete_user_from_cc(user)
+      raise "The given user is nil!" if user.nil?
+      user.delete!
     end
 
   end
